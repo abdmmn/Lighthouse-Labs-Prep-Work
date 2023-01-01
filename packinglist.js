b1a82c548f177d1aff7c3a4d1002a9d772014d90
abdmmn <abduljmemon@gmail.com>
2023-01-01 16:41:10 +0000

    same packing list array but used WHILE loop to list everything

--- packinglist.js
+++ packinglist.js
@@ -4,3 +4,14 @@ console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+
+
+
+
+//same thing as above^ but using while loop instead
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
